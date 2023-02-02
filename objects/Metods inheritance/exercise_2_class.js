ea392d1bdb498e1d9658e88ba4c1473e78a1f9ec
DESKTOP-EF2VHJM\ivana <dobrevolje1@gmail.com>
2023-02-02 14:02:40 +0100

    update

--- objects/Metods inheritance/exercise_2_class.js
+++ objects/Metods inheritance/exercise_2_class.js
@@ -1,3 +1,4 @@
+
 class Book {
 constructor (title, autor, copyrightDate, ISBN, numberOfpages, numberOfchecked, discarded ) {
     this.title = title
@@ -48,57 +49,106 @@ let novel_1 = new Novel ("When Nietzsche wept", "Irvin Jalom",1992, "978-92-9505
 
 
 novel_1.checkedOut(99)
-
-
 console.log(novel_1.checkedOutThan100())
-/*
-class Calculator {
-    constructor(stack) {
-        this.stack = []
-    }
-  plus = function (num1, num2, operation) {
-    operation = num1+num2
-    return this.stack.push(operation)   
 
+
+
+class StackCalc {
+  constructor() {
+    this.stack = []
+  }
+
+  run(instructions) {
+    if (instructions === '') {
+      return 0
+    }
+    const inst_arr = instructions.split(' ');
+    for (let i = 0; i < inst_arr.length; i++) {
+      const inst = inst_arr[i]
+      if (inst === '+') {
+        this.add()
+      } else if (inst === '-') {
+        this.subtract()
+      } else if (inst === '*') {
+        this.multiply()
+      } else if (inst === '/') {
+        this.divide()
+      } else if (inst === 'DUP') {
+        this.duplicate()
+      } else if (inst === 'POP') {
+        this.pop()
+      } else if (!isNaN(inst)) {
+        this.push(parseInt(inst));
+      } else {
+        return `Invalid instruction: ${inst}`
+      }
+    }
+    return this.getValue()
+  }
+
+  add() {
+    const num1 = this.stack.pop()
+    const num2 = this.stack.pop()
+    this.stack.push(num1 + num2)
+  }
+
+  subtract() {
+    const num1 = this.stack.pop()
+    const num2 = this.stack.pop()
+    this.stack.push(num1 - num2)
+  }
+
+  multiply() {
+    const num1 = this.stack.pop()
+    const num2 = this.stack.pop()
+    this.stack.push(num1 * num2)
+  }
+
+  divide() {
+    const num1 = this.stack.pop()
+    const num2 = this.stack.pop()
+    this.stack.push(num1 / num2)
+  }
+
+  duplicate() {
+    const num = this.stack[this.stack.length - 1]
+    this.stack.push(num)
+  }
+
+  pop() {
+    this.stack.pop()
+  }
+
+  push(num) {
+    this.stack.push(num)
+  }
+
+  getValue() {
+    return this.stack[this.stack.length - 1]
   }
 }
+const calculator_1 = new StackCalc()
+console.log(calculator_1.run('5 6 +'))
+console.log(calculator_1.run('3 DUP +'))
+console.log(calculator_1.run('6 5 5 7 * - /'))
+console.log(calculator_1.run('x y +'))
 
-let test_1 = new Calculator ()
-test_1.plus("2 3 +")
-console.log(test_1.stack)
-*/
 
 class CoffeShop {
-    constructor(name_) {
-        this.name_ = name_
-        this.menu = [
-            {item: "tuna sandwich", type: "food", price: 3.50},
-            {item: "ham and cheese sandwich", type: "food", price: 4.00},
-            {item: "bacon and egg", type: "food", price: 4.50},
-            {item: "steak", type: "food", price: 6.50},
-            {item: "hamburger", type: "food", price: 5.00},
-            {item: "cinnamon roll", type: "food", price: 2.00},
-            {item: "orange juice", type: "drink", price: 1.50},
-            {item: "lemonade", type: "drink", price: 1.25},
-            {item: "cranberry juice", type: "drink", price: 2.00},
-            {item: "pineapple juice", type: "drink", price: 2.50},
-            {item: "lemon iced tea", type: "drink", price: 2.50},
-            {item: "vanilla chai latte", type: "drink", price: 3.50},
-            {item: "hot chocolate", type: "drink", price: 2.75},
-            {item: "iced coffee", type: "drink", price: 2.00}
-              ]
+    constructor(name, menu, orders) {
+        this.name = name
+        this.menu = menu
         this.orders = []
-
     }
-    addOrder = function (some_item) {
-        
-    let order = this.menu.filter(el => some_item === el.item);
-    if (order) {
-      this.orders.push(order)
-      return "Order added!"
-    } else return this.name_ + " coffee shop does not sell " + some_item
-  
-}
+
+    addOrder = function(some_item) {
+        if (this.menu.filter(m => m.item == some_item)) {
+            this.orders.push(some_item);
+            return "Order added!"
+          } else return console.log("This item is currently unavailable!");
+        }
+      
+
     fulfillOrder = function () {
         if (this.orders.length === 0) {
             return "All orders have been fulfilled!"
@@ -115,33 +165,65 @@ class CoffeShop {
       }
     
     dueAmount = function () {
-        let sum = 0
-        this.orders.forEach(function(element){
-         sum+=element.price;
-        })
-      
-          return  sum.toFixed(2)
+        let total = 0
+    this.orders.forEach(order => {
+      let amount = this.menu.find(el => el.item == order)
+      total += amount.price
+    })
+    return total.toFixed(2)
         
-}   
-   cheapestItem = function() {
-    let cheapest = this.menu[0];
+    }   
+    cheapestItem = function() {
+    let cheapest = this.menu[0]
     for (let i = 1; i < this.menu.length; i++) {
       if (this.menu[i].price < cheapest.price) {
-        cheapest = this.menu[i];
+        cheapest = this.menu[i]
       }
     }
-    return cheapest.item;
-  }
-}
+    return cheapest.item
+    }
 
- let coffes_shop = new CoffeShop ("Arabic-Shop")
+    drinkOnly = function () {
+       
+        return this.menu.filter((el) => el.type == "drink")
+        .map((el) => el.item)
+        
+    }
+
+    foodOnly = function () {
+       
+    return this.menu.filter((el) => el.type == "food")
+    .map((el) => el.item)
+    
+    }
+    }
+
+
+ let coffes_shop = new CoffeShop ("Arabic-Shop",[
+    {item: "tuna sandwich", type: "food", price: 3.50},
+    {item: "ham and cheese sandwich", type: "food", price: 4.00},
+    {item: "bacon and egg", type: "food", price: 4.50},
+    {item: "steak", type: "food", price: 6.50},
+    {item: "hamburger", type: "food", price: 5.00},
+    {item: "cinnamon roll", type: "food", price: 2.00},
+    {item: "orange juice", type: "drink", price: 1.50},
+    {item: "lemonade", type: "drink", price: 1.25},
+    {item: "cranberry juice", type: "drink", price: 2.00},
+    {item: "pineapple juice", type: "drink", price: 2.50},
+    {item: "lemon iced tea", type: "drink", price: 2.50},
+    {item: "vanilla chai latte", type: "drink", price: 3.50},
+    {item: "hot chocolate", type: "drink", price: 2.75},
+    {item: "iced coffee", type: "drink", price: 2.00}
+      ])
   
- coffes_shop.addOrder("steak")
- coffes_shop.addOrder("orange juice")
+
  console.log( coffes_shop.addOrder("orange juice"))
+ console.log( coffes_shop.addOrder("iced coffee"))
  console.log( coffes_shop.orders)
  console.log( coffes_shop.fulfillOrder())
  console.log( coffes_shop.listOfOrders())
  console.log( coffes_shop.dueAmount())
  console.log( coffes_shop.cheapestItem())
+ console.log( coffes_shop.drinkOnly())
+ console.log( coffes_shop.foodOnly())
 
