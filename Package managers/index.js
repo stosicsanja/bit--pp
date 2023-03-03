42aac8cc89e468d490ab0dfb80a0a127d3e6243e
DESKTOP-EF2VHJM\ivana <dobrevolje1@gmail.com>
2023-03-03 12:47:19 +0100

    update

---
+++ Package managers/index.js
@@ -0,0 +1,9 @@
+const figlet = require("figlet")
+
+console.log (
+    figlet.textSync("Hello World","Slant", {horizontalLayout:"full"})
+)
+
+console.log (
+    figlet.textSync("Hello World","Sub-Zero", {horizontalLayout:"full"})
+)
